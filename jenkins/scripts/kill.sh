7418889ecbdd5e8c4acb132b5230b23823860340
dumln86 <dumln86@gmail.com>
2018-02-06 10:43:56 +0700

    update kill.sh11111111111111111

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,6 +1,6 @@
 ##!/usr/bin/env sh
 #!/bin/bash
-echo 'TTTTTTTTTTTTTTTTTTTTTTTTTTThe following command terminates the "npm start" process using its PID'
+echo '2222222222222222TTTTTTTTTTTTTTTTTTTTTTTTTTThe following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when either'
 echo '"deliver-for-development.sh" or "deliver-for-development.sh" was executed.'
 #set -x
